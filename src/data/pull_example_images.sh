e858d5d8e324f71f21a59da0b947f8ab485fe5da
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2021-12-07 11:01:49 +0100

    also pulling bortezomib

--- src/data/pull_example_images.sh
+++ src/data/pull_example_images.sh
@@ -4,10 +4,16 @@ WD=$PWD
 # E001 - io3-011-proof_of_robotics
 # pulling brightfield data
 cd data/raw/PROMISE/incoming/PROMISE-4x-1t-1z-Brightfield-fast
+# DMSO
 cp --parents PROMISE-4x-1t-1z-Brightfield-fast_HC9000E001D080B001*/C_9_wv_TL_Brightfield_dsRed.tif ../../../../../data/interim/automatic_microscopy/HC9000E001
+# Bortezomib
+cp --parents PROMISE-4x-1t-1z-Brightfield-fast_HC9000E001D080B001*/C_21_wv_TL_Brightfield_dsRed.tif ../../../../../data/interim/automatic_microscopy/HC9000E001
 cd $WD
 # pulling flourescence data
 cd data/raw/PROMISE/incoming/PROMISE-20x-6t-c-5z_IO
+# DMSO
 cp --parents PROMISE-20x-6t-c-5z_IO_HC9000E001D080B001*/C_9_fld_5* ../../../../../data/interim/automatic_microscopy/HC9000E001
+# Bortezomib
+cp --parents PROMISE-20x-6t-c-5z_IO_HC9000E001D080B001*/C_21_fld_5* ../../../../../data/interim/automatic_microscopy/HC9000E001
 cd $WD  
 echo copy completed
